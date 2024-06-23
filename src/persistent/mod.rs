8c96345bbfb25ccfcd2280168a67394a494e4bc9
stephen <u6269649@anu.edu.au>
2024-06-23 12:58:57 +1000

    ENH: init_mongo_connection takes conn string instead

--- src/persistent/mod.rs
+++ src/persistent/mod.rs
@@ -33,25 +33,24 @@ pub struct PersistentLayer {
     pub mongo_config: MongoDbConfig,
 }
 
-pub async fn init_mongo_connection(config: &MongoDbConfig) -> Result<Client> {
-    let connection_string = format!(
-        "mongodb://{}:{}@{}:{}/{}?authSource={}",
-        &config.mongo_username,
-        &config.mongo_password,
-        &config.mongo_host,
-        &config.mongo_port,
-        &config.mongo_db_name,
-        &config.mongo_auth_source,
-    );
+pub async fn init_mongo_connection(
+    connection_string: &str,
+    max_pool_size: Option<u32>,
+) -> Result<Client> {
+    let mut options = ClientOptions::parse(connection_string).await?;
 
-    let mut options = ClientOptions::parse(&connection_string).await?;
-
-    options.max_pool_size = Some(config.mongo_max_connections);
+    if let Some(max_pool_size) = max_pool_size {
+        options.max_pool_size = Some(max_pool_size);
+    }
 
     let client = Client::with_options(options)?;
 
-    // test client connection
-    let db = client.database(&config.mongo_db_name);
+    let db_name = options
+        .default_database
+        .clone()
+        .expect("database name must be specified in the connection string");
+
+    let db = client.database(&db_name);
     db.run_command(doc! { "ping": 1 }, None).await?;
 
     Ok(client)
